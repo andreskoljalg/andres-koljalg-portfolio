34cc8e274b0bc2d96ce2d329131ed370cd0e9895
Fredy Ait <aitfredy@gmail.com>
2022-03-16 20:13:43 +0200

    animation function

---
+++ script.js
@@ -0,0 +1,11 @@
+const title = document.getElementById('title');
+const tDiv = document.getElementById('title-div');
+const links = document.getElementById('links');
+const tImage = document.getElementById('title-image');
+
+window.onload = function(){
+    title.style.opacity = "1";
+    tDiv.classList.remove('translate-x-full');
+    links.style.opacity = "1";
+    tImage.style.opacity = "1";
+}
