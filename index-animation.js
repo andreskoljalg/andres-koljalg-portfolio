9e3751bf25931f7c9f2ecfdc7791056f609d39dc
Fredy Ait <aitfredy@gmail.com>
2022-03-25 11:39:34 +0200

    mobile view & opening animation integration

--- index-animation.js
+++ index-animation.js
@@ -1,11 +1,8 @@
-const title = document.getElementById('title');
-const tDiv = document.getElementById('title-div');
-const links = document.getElementById('links');
-const tImage = document.getElementById('title-image');
+const element = document.querySelectorAll('[id=fade]')
 
 window.onload = function(){
-    title.style.opacity = "1";
-    tDiv.classList.remove('translate-x-full');
-    links.style.opacity = "1";
-    tImage.style.opacity = "1";
+    element.forEach((el) => {
+        el.style.opacity = 1;
+        el.classList.remove("translate-x-full");
+    });
 }
