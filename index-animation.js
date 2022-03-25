194a86f641fed9dd029af3d14c7cdd0ab26da003
Fredy Ait <aitfredy@gmail.com>
2022-03-25 13:13:37 +0200

    animation, mobile view fixes & improvements

--- index-animation.js
+++ index-animation.js
@@ -1,4 +1,7 @@
-const element = document.querySelectorAll('[id=fade]')
+const element = document.querySelectorAll('[id=fade]');
+const menuToggle = document.querySelectorAll('[id=menuToggle]');
+let menuToggled = false;
+const menu = document.getElementById('menu');
 
 window.onload = function(){
     element.forEach((el) => {
@@ -6,3 +9,15 @@ window.onload = function(){
         el.classList.remove("translate-x-full");
     });
 }
+
+menuToggle.forEach((el) => {
+    el.addEventListener('click', function(event){
+        if(!menuToggled){
+            menuToggled = true;
+            menu.classList.remove("translate-x-full");
+        } else {
+            menuToggled = false;
+            menu.classList.add("translate-x-full");
+        }
+    })
+});
