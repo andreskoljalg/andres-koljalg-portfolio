02fdfa9a47e86de29375c0d42cb978eecbb8c9ce
Fredy Ait <aitfredy@gmail.com>
2022-04-17 18:59:26 +0300

    Simple spam protection

--- contactform.php
+++ contactform.php
@@ -2,13 +2,15 @@
 
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
+    $honeyPotMail = $_POST['email'];
+    $honeyPotWebsite = $_POST['website'];
     $subject = "New message from portfolio";
-    $mailFrom = $_POST['mail'];
+    $mailFrom = $_POST['emanueladdresshaha'];
     $message = $_POST['message'];
     $mailTo = "server@tak21fredyait.itmajakas.ee";
     $headers = "From ".$name." at ".$mailFrom.". Sent using portfolio contact form.";
 
-    if (empty($name)) {
+    if (empty($honeyPotMail) && empty($honeyPotWebsite)) {
         mail($mailTo, $subject, $message, $headers);
         header("Location: contact.html");
     } else {
