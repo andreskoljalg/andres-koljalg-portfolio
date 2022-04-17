008549df07c7a3369b739aa8f4f0371faf98058a
Fredy Ait <aitfredy@gmail.com>
2022-04-17 18:32:50 +0300

    vulnerability fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['*.{html,js,php}', './assets/projects/**/*.{html,js,php}'],
+  content: ['*.{html,js,php}', './projects/**/*.{html,js,php}'],
   theme: {
     extend: {},
   },
