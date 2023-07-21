2cd842e13037d0bfaecb3e9c74e7e4c4ab9a7ce1
Fredy Ait <aitfredy@gmail.com>
2023-07-21 15:56:05 +0300

    Added assets, fixed index and seperated header

---
+++ src/components/MobileHeader.vue
@@ -0,0 +1,38 @@
+<script setup>
+let navShown = false;
+
+function toggleMenu() {
+    const nav = document.getElementById('nav')
+    if(!navShown) {
+        nav.classList.remove('translate-x-full')
+        navShown = true;
+    } else {
+        nav.classList.add('translate-x-full')
+        navShown = false;
+    }
+}
+</script>
+
+<template>
+  <div class="fixed translate-x-full h-screen duration-300 ease-in-out w-screen justify-center items-center flex flex-col text-3xl font-['Times_New_Roman'] lg:hidden bg-white" id="nav">
+      <a href="javascript:delay('/about.html')">About</a>
+      <p class="w-fit">&#8226</p>
+      <a href="javascript:delay('/projects.php')">Projects</a>
+      <p class="w-fit">&#8226</p>
+      <a href="javascript:delay('/journal.php')">Journal</a>
+      <p class="w-fit">&#8226</p>
+      <a href="javascript:delay('/contact.html')" class="mb-6">Contact</a>
+      <div class="w-12 h-12 flex justify-center items-center" @click="toggleMenu()">
+          <div class="w-12 fixed h-2 bg-black rotate-45"></div>
+          <div class="w-12 fixed h-2 bg-black -rotate-45"></div>
+      </div>
+  </div>
+  <header class="flex h-[8vh] justify-between items-center p-2 lg:hidden">
+      <a href="" class="w-44"><img src="../assets/svgs/ak_mainlogotitle.svg" class="h-full" alt="ak_mainlogotitle"></a>
+      <div class="w-12 flex justify-between items-end py-2 flex-col h-full max-h-12" @click="toggleMenu()">
+          <div class="h-1/6 w-1/3 bg-black"></div>
+          <div class="h-1/6 w-2/3 bg-black"></div>
+          <div class="h-1/6 w-full bg-black"></div>
+      </div>
+  </header>
+</template>
---
+++ src/views/Index.vue
@@ -0,0 +1,28 @@
+<script setup>
+import MobileHeader from '../components/MobileHeader.vue';
+</script>
+
+<template>
+    <MobileHeader />
+    <div class="h-[92vh] p-12 w-screen flex flex-col items-center justify-center lg:hidden">
+        <img src="../assets/images/title_image_rotated.png" class="w-[50vh] mb-8" alt="title_image_rotated">
+        <a href="javascript:delay('/contact.html')" class="font-['Times_New_Roman'] text-2xl w-fit">hello@andreskoljalg.com</a>
+    </div>
+    <div class="justify-center gap-20 h-screen items-center p-12 hidden lg:flex">
+        <div class="w-fit" id="slide">
+            <img src="../assets/svgs/ak_mainlogotitle.svg" alt="ak_mainlogotitle" id="slide">
+            <div class="flex gap-2 justify-between text-3xl xl:text-4xl font-['Times_New_Roman']">
+                <a href="javascript:delay('/about.html')">About</a>
+                <p class="w-fit">&#8226</p>
+                <a href="javascript:delay('/projects.php')">Projects</a>
+                <p class="w-fit">&#8226</p>
+                <a href="javascript:delay('/journal.php')">Journal</a>
+                <p class="w-fit">&#8226</p>
+                <a href="javascript:delay('/contact.html')">Contact</a>
+            </div>
+        </div>
+        <div>
+            <img src="../assets/images/title_image.png" alt="title_image">
+        </div>
+    </div>
+</template>
