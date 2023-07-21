42adba82ee293afebe1339aeda625f6a5df29cb2
Fredy Ait <aitfredy@gmail.com>
2023-07-21 20:14:17 +0300

    Router transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <router-view v-slot="{ Component, route }">
+    <transition name="route" mode="out-in">
+      <div :key="route.name">  
+        <component :is="Component"></component>
+      </div>
+    </transition>
+  </router-view>
+
 </template>
+<style scoped>
+/* ROUTER TRANSITIONS */
+.route-enter-from {
+  opacity: 0;
+  /* transform: translateX(100px); */
+}
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+.route-leave-to {
+  opacity: 0;
+  /* transform: translateX(-100px); */
+}
+.route-leave-active {
+  transition: all 0.3s ease-in;
+}
+</style>
