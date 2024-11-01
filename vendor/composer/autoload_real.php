3f5275cab1ee9541bf3d140902d61dceff01a68d
andreskoljalg <103831261+andreskoljalg@users.noreply.github.com>
2024-11-01 22:10:11 +0000

    Fixed vercel.json and build succeeded locally

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit4b55c7ef33235a0353c1b6b079f86964
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit4b55c7ef33235a0353c1b6b079f86964', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit4b55c7ef33235a0353c1b6b079f86964', 'loadClassLoader'));
